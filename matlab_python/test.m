a69c779a98fea15282d807d0ad20a6385738f5be
psyphy3 <65641172+psyphy3@users.noreply.github.com>
2024-04-30 23:20:52 +0200

    Update test.m

--- matlab_python/test.m
+++ matlab_python/test.m
@@ -1,44 +1,23 @@
 %% Code to test Nicslab XDAC-40U-R4G8
+
 tic
 %% ==Initialisations=======
-key="BEL-ULB40c001";
+key="YOUR_KEY";
 inter="interface.py";
 x1=Xdac(key,inter);
 
 %% ==First time procedures===
-% x1.unlock();
-% x1.setZ();
-% x1.setVth('all',4);
-% x1.setCth('all',3);
-% x1.CCmode(linspace(1,20,20),1);
+x1.unlock();
+x1.setZ();
+x1.setVth('all',4);
+x1.setCth('all',3);
+x1.CCmode('all',6,2);
 
 % ===Main body=========
 
-%channel 36 is bad
+X1.setV(2,3);
+V=x1.getV(2);
 
-% V_array=zeros(1,40);
-% C_array=zeros(1,40);
-% R_array=zeros(1,40);
-% for i=1:40
-%     ch=i;
-%     x1.setV(ch,3);+
-%     x1.setC(ch,1);
-% 
-%     pause(0.1);
-% 
-%     V=x1.getV(ch);
-%     C=x1.getC(ch);
-%     R=V/C;
-% 
-%     V_array(i)=V;
-%     C_array(i)=C;
-%     R_array(i)=R;
-% end
-
-
-
-% histogram(R_array,8);
-% plot(R_array);
 
 
 %% ==Shutdown procedures====
