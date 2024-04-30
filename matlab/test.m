325f346682840ee8503642c5929c952c3c34d01b
psyphy3 <65641172+psyphy3@users.noreply.github.com>
2024-04-30 23:21:44 +0200

    Update test.m

--- matlab/test.m
+++ matlab/test.m
@@ -1,1 +1,28 @@
+%% Code to test Nicslab XDAC-40U-R4G8
 
+tic
+%% ==Initialisations=======
+key="YOUR_KEY";
+ip="YOUR_IP";
+x1=Xdac(ip,key);
+
+%% ==First time procedures===
+x1.unlock();
+x1.setZ();
+x1.setVth('all',4);
+x1.setCth('all',3);
+x1.CCmode('all',6,2);
+
+% ===Main body=========
+
+X1.setV(2,3);
+V=x1.getV(2);
+
+
+
+%% ==Shutdown procedures====
+x1.lock();
+x1.shutdown();
+
+%% =================
+toc
